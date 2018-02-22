233abe8c91ec0f8f1ede22bc88635f026aff63c8
Serhiy Chornobay <sergij.chornobay@lemberg.co.uk>
2018-02-22 13:13:03 +0200

    2945370 by serhiy-chornobay: Disable autosize for textarea only for mobile

--- themes/socialbase/components/02-atoms/form-controls/autosize.init.js
+++ themes/socialbase/components/02-atoms/form-controls/autosize.init.js
@@ -1,22 +1,27 @@
 (function ($) {
 
-  // Drupal.behaviors.textareaAutogrow = {
-  //   attach: function (context, settings) {
-  //
-  //     // Attach autosize listener.
-  //     autosize($('.form-control--autogrow'));
-  //   }
-  // }
-  //
-  // Drupal.behaviors.textareaFocus = {
-  //   attach: function (context, settings) {
-  //     $('textarea:last').focusin(function() {
-  //       $(this).parents('.main-container').toggleClass('open-keyboard');
-  //     });
-  //     $('textarea:last').focusout(function() {
-  //       $(this).parents('.main-container').toggleClass('open-keyboard');
-  //     });
-  //   }
-  // }
+  Drupal.behaviors.textareaAutogrow = {
+    attach: function (context, settings) {
+
+      // Toggles inline display of profile dropdown menu items.
+      var autosizeResizeUpdate = function () {
+        var viewportWidth = window.innerWidth;
+        var mobileUpBreakpoint = 600;
+
+        if (viewportWidth >= mobileUpBreakpoint) {
+          // Attach autosize listener.
+          autosize($('.form-control--autogrow'));
+        }
+
+      };
+
+      // Executed on document load and window resize.
+      autosizeResizeUpdate();
+      $(window).resize(_.debounce(function(){
+        autosizeResizeUpdate()
+      }, 500));
+
+    }
+  }
 
 })(jQuery);
