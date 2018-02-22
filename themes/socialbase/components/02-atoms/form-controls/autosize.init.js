ca7b6071afa5a62fab31de4aca09f6d3914edb45
Serhiy Chornobay <sergij.chornobay@lemberg.co.uk>
2018-02-22 13:58:14 +0200

    2945370 by serhiy-chornobay: Try set autosize only when textarea is focus

--- themes/socialbase/components/02-atoms/form-controls/autosize.init.js
+++ themes/socialbase/components/02-atoms/form-controls/autosize.init.js
@@ -3,23 +3,29 @@
   Drupal.behaviors.textareaAutogrow = {
     attach: function (context, settings) {
 
-      // Toggles inline display of profile dropdown menu items.
-      var autosizeResizeUpdate = function () {
-        var viewportWidth = window.innerWidth;
-        var mobileUpBreakpoint = 600;
 
-        if (viewportWidth >= mobileUpBreakpoint) {
-          // Attach autosize listener.
-          autosize($('.form-control--autogrow'));
-        }
+      var ta = $('.form-control--autogrow');
+      ta.addEventListener('focus', function(){
+        autosize(ta);
+      });
 
-      };
-
-      // Executed on document load and window resize.
-      autosizeResizeUpdate();
-      $(window).resize(_.debounce(function(){
-        autosizeResizeUpdate()
-      }, 500));
+      // // Toggles inline display of profile dropdown menu items.
+      // var autosizeResizeUpdate = function () {
+      //   var viewportWidth = window.innerWidth;
+      //   var mobileUpBreakpoint = 600;
+      //
+      //   if (viewportWidth >= mobileUpBreakpoint) {
+      //     // Attach autosize listener.
+      //     autosize($('.form-control--autogrow'));
+      //   }
+      //
+      // };
+      //
+      // // Executed on document load and window resize.
+      // autosizeResizeUpdate();
+      // $(window).resize(_.debounce(function(){
+      //   autosizeResizeUpdate()
+      // }, 500));
 
     }
   }
