edc82101e13fb2f410a9e3a91655987c848db1ea
Serhiy Chornobay <sergij.chornobay@lemberg.co.uk>
2018-02-22 15:08:31 +0200

    2945370 by serhiy-chornobay: Try set autosize only when textarea is focus

--- themes/socialbase/components/02-atoms/form-controls/autosize.init.js
+++ themes/socialbase/components/02-atoms/form-controls/autosize.init.js
@@ -5,27 +5,11 @@
 
 
       var ta = $('.form-control--autogrow');
-      ta.addEventListener('focus', function(){
+      ta.focus(function() {
         autosize(ta);
       });
 
-      // // Toggles inline display of profile dropdown menu items.
-      // var autosizeResizeUpdate = function () {
-      //   var viewportWidth = window.innerWidth;
-      //   var mobileUpBreakpoint = 600;
-      //
-      //   if (viewportWidth >= mobileUpBreakpoint) {
-      //     // Attach autosize listener.
-      //     autosize($('.form-control--autogrow'));
-      //   }
-      //
-      // };
-      //
-      // // Executed on document load and window resize.
-      // autosizeResizeUpdate();
-      // $(window).resize(_.debounce(function(){
-      //   autosizeResizeUpdate()
-      // }, 500));
+
 
     }
   }
