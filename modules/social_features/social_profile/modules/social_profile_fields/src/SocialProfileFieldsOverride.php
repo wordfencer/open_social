963dbf2f6f52f0b21307db84c91fb18d60169339
Jochem van Nieuwenhuijsen <jochem@goalgorilla.com>
2018-12-12 14:56:56 +0100

    ISHN-19 by jochemvn: Refactor code to use DI

--- modules/social_features/social_profile/modules/social_profile_fields/src/SocialProfileFieldsOverride.php
+++ modules/social_features/social_profile/modules/social_profile_fields/src/SocialProfileFieldsOverride.php
@@ -3,6 +3,7 @@
 namespace Drupal\social_profile_fields;
 
 use Drupal\Core\Cache\CacheableMetadata;
+use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Config\ConfigFactoryOverrideInterface;
 use Drupal\Core\Config\StorageInterface;
 
@@ -16,16 +17,32 @@ use Drupal\Core\Config\StorageInterface;
 class SocialProfileFieldsOverride implements ConfigFactoryOverrideInterface {
 
   /**
+   * The config factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * Constructs the configuration override.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The Drupal configuration factory.
+   */
+  public function __construct(ConfigFactoryInterface $config_factory) {
+    $this->configFactory = $config_factory;
+  }
+
+  /**
    * Returns config overrides.
    */
-  public function loadOverrides($names) {
+  public function loadOverrides($names) :array {
     $overrides = [];
-    $config_factory = \Drupal::service('config.factory');
 
     // Add field_group and field_comment_files.
     $config_name = 'core.entity_form_display.profile.profile.default';
     if (in_array($config_name, $names)) {
-      $config = $config_factory->getEditable($config_name);
+      $config = $this->configFactory->getEditable($config_name);
 
       // Add the nick name field to the profile.
       $overrides[$config_name]['content']['field_profile_nick_name'] = [
@@ -40,7 +57,7 @@ class SocialProfileFieldsOverride implements ConfigFactoryOverrideInterface {
       ];
 
       // If there is a profile names and image field_group we move the field.
-      $third_party = $config->get('third_party_settings');
+      $third_party = $config->getOriginal('third_party_settings');
       if (isset($third_party['field_group']['group_profile_names_image'])) {
         $third_party['field_group']['group_profile_names_image']['children'][] = 'field_profile_nick_name';
 
